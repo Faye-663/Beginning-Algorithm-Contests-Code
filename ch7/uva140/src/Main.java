a5dc433cc0e5decd385a81ee35fc3fdda2d27a3d
Faye-663 <53008307+Faye-663@users.noreply.github.com>
2020-08-23 16:30:07 +0800

    ch7

---
+++ ch7/uva140/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		while(in.hasNext()){
+			String line = in.nextLine();
+			if("#".equals(line)){
+				break;
+			}
+			
+			
+		}
+
+	}
+
+}
